7d4c6f033733ef20d7ed3c5124b8d456e1150ce1
ErnestGeo24 <ernest1.avasi@gmail.com>
2024-05-30 12:57:19 +0000

    punto2

---
+++ anifru/src/app/generic/generic.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generic',
+  templateUrl: './generic.component.html',
+  styleUrls: ['./generic.component.css']
+})
+export class GenericComponent {ù
+  
+  constructor(private route: ActivatedRoute) {
+    this.route.paramMap.subscribe(this.getRouterParam);
+    }
+  getRouterParam = (params: ParamMap) =>
+    {
+      let uri_param = params.get('id'); //Ottengo l'id dalla ParamMap
+      console.log (uri_param); //Stampo su console
+      //this.service.getTrack()
+    }
+  
+}
